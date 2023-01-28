b0660ca0b78e7e5812abff4b337185129f0f41b8
Alairton Pereira <alairtonpereira.dev@gmail.com>
2023-01-28 01:37:48 +0044

    Configuração do POST dos dados Validação dos dados: nome, email, senha e confirmar senha

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,5 +3,19 @@ from django.http import HttpResponse
 def cadastro(request):
     # return HttpResponse('Olá estou no cadastro')
     # A função render() recebe a requisição do usuário
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        # print(nome, email, senha, confirmar_senha)
+        # Validação dos campos do formulario
+        if  len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
+            return render(request, 'cadastro.html')
+        if senha != confirmar_senha:
+            return render(request, 'cadastro.html')
+
+        return HttpResponse(f'{nome}, {email}, {senha}, {confirmar_senha}')
 
